5691a9b7d32973e3b1ba6cd7b10213d98dab3475
Juanito970 <juan.angel19@hotmail.com>
2021-07-18 14:45:12 -0500

    Initial commit
    Init repo for dot files:
    - Emacs
    - Bashrc
    - Zshrc

---
+++ .doom.d/config.el
@@ -0,0 +1,91 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Juan Angel Gonzalez"
+      user-mail-address "juan.angel19@hotmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-nord-light)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "/Users/juangonzalez/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/")
+(setq org-log-done 'time)
+ (let* ((variable-tuple (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+                                 ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+                                 ((x-list-fonts "Verdana")         '(:font "Verdana"))
+                                 ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+                                 (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+           (base-font-color     (face-foreground 'default nil 'default))
+           (headline           `(:inherit default :weight bold :foreground ,base-font-color))
+           )
+
+      (custom-theme-set-faces 'user
+                              `(org-level-8 ((t (,@headline ,@variable-tuple))))
+                              `(org-level-7 ((t (,@headline ,@variable-tuple))))
+                              `(org-level-6 ((t (,@headline ,@variable-tuple))))
+                              `(org-level-5 ((t (,@headline ,@variable-tuple))))
+                              `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.05))))
+                              `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.10))))
+                              `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.15))))
+                              `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.25))))
+                              `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil)))))
+      )
+
+;; Mathematica config
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type `relative)
+
+;; Key bindings
+(define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+(define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+(remove-hook 'doom-first-input-hook #'evil-snipe-mode)
+
+;; PlantUML config (Test)
+(setq org-plantuml-jar-path "Users/juangonzalez/plantuml.jar")
+(setq plantuml-jar-path (expand-file-name "/Users/juangonzalez/plantuml.jar"))
+
+
+;;
+;; Auto-complete settings
+(setq company-minimum-prefix-length 3
+      company-idle-delay 0.800)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
---
+++ .doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/Users/juangonzalez/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/Agenda.org" "/Users/juangonzalez/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/diary.org" "/Users/juangonzalez/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/journal.org" "/Users/juangonzalez/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/notes.org" "/Users/juangonzalez/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/todo.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
